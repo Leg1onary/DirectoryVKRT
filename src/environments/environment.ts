63a9ed77266142420090f0ab3bc26ce8481c9631
Sergey Sholokhov <shserg94@gmail.com>
2019-02-19 23:33:18 +0300

    private info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: 'AIzaSyA2xSeQFTjYH4KXXqVTEddTJJDhJ4maD5s',
-    authDomain: 'directoryvkrt.firebaseapp.com',
-    databaseURL: 'https://directoryvkrt.firebaseio.com',
-    projectId: 'directoryvkrt',
-    storageBucket: 'directoryvkrt.appspot.com',
-    messagingSenderId: '493632299253'
+
   }
 };
 
